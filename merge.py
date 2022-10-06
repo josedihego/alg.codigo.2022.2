ff689ae5f48ecc78947eb6554fe503f73df97eaa
josedihego <josedihego@gmail.com>
2022-10-06 17:13:07 -0300

    merge finalizado

--- merge.py
+++ merge.py
@@ -11,14 +11,21 @@ def merge(A,p,q,r):
     i =0
     j = 0
     for k in range(p,r+1):
-        if L[i] <= R[j]:
-            A[k] =  L[i]
-            i = i + 1
+        if(i==len(L) or j == len(R)):
+            if(i==len(L) and j < len(R)):
+                A[k] = R[j]
+                j =  j +1
+            elif(j==len(R) and i < len(L)):
+                A[k] = L[i]
+                i = i +1
         else:
-            A[k] =  R[j]
-            j = j + 1
+            if L[i] <= R[j]:
+                A[k] =  L[i]
+                i = i + 1
+            else:
+                A[k] =  R[j]
+                j = j + 1
     return A
 
-
-
+print(merge(A,0,3,7))
 assert merge(A,0,3,7)== [-1, 0, 6, 8, 15, 16, 22, 24]
