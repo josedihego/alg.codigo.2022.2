a4b4d09cf85ed7968f368ce659b6bef16877b8c2
josedihego <josedihego@gmail.com>
2022-10-06 17:00:10 -0300

    merge ainda não finalizado

--- merge.py
+++ merge.py
@@ -4,7 +4,21 @@ A = [-1,8,15,24,0,6,16,22]
 # codigo 
 
 def merge(A,p,q,r):
-    return []
+    n1 = q - p +1
+    n2 =  r - q
+    L = A[p:q+1]
+    R = A[q+1:r+1]
+    i =0
+    j = 0
+    for k in range(p,r+1):
+        if L[i] <= R[j]:
+            A[k] =  L[i]
+            i = i + 1
+        else:
+            A[k] =  R[j]
+            j = j + 1
+    return A
+
 
 
 assert merge(A,0,3,7)== [-1, 0, 6, 8, 15, 16, 22, 24]
