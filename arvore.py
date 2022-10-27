9a9e86f48fab04fb42fe99eadbbaafceefde25f8
josedihego <josedihego@gmail.com>
2022-10-27 17:35:36 -0300

    inicio arvore

---
+++ arvore.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.p = None
+        self.left = None
+        self.right = None
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
