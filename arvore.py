83e7cba980f5ff2fe720ca7232c67ef0727144b0
josedihego <josedihego@gmail.com>
2022-10-27 17:56:50 -0300

    arvore com insert

--- arvore.py
+++ arvore.py
@@ -9,3 +9,31 @@ class Tree:
     def __init__(self):
         self.root = None
 
+    def insert(self, z):
+        y = None
+        x = self.root
+        while x!=None:
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        z.p = y
+        if y == None:
+            self.root =  z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+
+
+minhaQueridaArvore = Tree()
+minhaRaizinha = Node(41)
+minhaQueridaArvore.root =  minhaRaizinha
+minhaQueridaArvore.insert(Node(55))
+minhaQueridaArvore.insert(Node(32))
+minhaQueridaArvore.insert(Node(65))
+minhaQueridaArvore.insert(Node(24))
+print('fim')
+
+
